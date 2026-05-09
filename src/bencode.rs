e4d4d5dcaafbdf6c28d9d76b47689f92e82d7663
angrypig555 <brny@brny.dev>
2026-05-09 09:05:00 +0100

    add bencode parser

--- src/bencode.rs
+++ src/bencode.rs
@@ -15,10 +15,89 @@
    limitations under the License.
 */
 
-struct bencode_decoded {
-    name: String,
+    /* 
+    pub enum Bencode {
+        String(Vec<u8>),
+        Integer(i64),
+        List(Vec<Bencode>),
+        Dict(BTreeMap<Vec<u8>, Bencode>),
+    }
+
+    pub fn decode_bencode(bencode_raw: String) -> Bencode {
+        let is_integer = false;
+        for c in bencode_raw.chars() {
+            if c == 'i' {
+                is_integer = true;
+            } else if is_integer == true {
+                Bencode::Integer((c))
+            }
+
+        }
+    }
+        */
+
+use serde::Deserialize;
+use serde_bencode::de;
+use serde_bytes::ByteBuf;
+
+#[derive(Debug, Deserialize)]
+struct Node(String, i64);
+
+#[derive(Debug, Deserialize)]
+struct File {
+    path: Vec<String>,
+    length: i64,
+    #[serde(default)]
+    md5sum: Option<String>,
 }
 
-pub fn decode_bencode() {
+#[allow(dead_code)]
+#[derive(Debug, Deserialize)]
+pub struct Info {
+    pub name: String,
+    pub pieces: ByteBuf,
+    #[serde(rename = "piece length")]
+    pub piece_length: i64,
+    #[serde(default)]
+    pub md5sum: Option<String>,
+    #[serde(default)]
+    pub length: Option<i64>,
+    #[serde(default)]
+    pub files: Option<Vec<File>>,
+    #[serde(default)]
+    pub private: Option<u8>,
+    #[serde(default)]
+    pub path: Option<Vec<String>>,
+    #[serde(default)]
+    #[serde(rename = "root hash")]
+    pub root_hash: Option<String>,
+}
 
+#[derive(Debug, Deserialize)]
+pub struct Torrent {
+    pub info: Info,
+    #[serde(default)]
+    pub announce: Option<String>,
+    #[serde(default)]
+    pub nodes: Option<Vec<Node>>,
+    #[serde(default)]
+    pub encoding: Option<String>,
+    #[serde(default)]
+    pub httpseeds: Option<Vec<String>>,
+    #[serde(default)]
+    #[serde(rename = "announce-list")]
+    pub announce_list: Option<Vec<Vec<String>>>,
+    #[serde(default)]
+    #[serde(rename = "creation date")]
+    pub creation_date: Option<i64>,
+    #[serde(rename = "comment")]
+    pub comment: Option<String>,
+    #[serde(default)]
+    #[serde(rename = "created by")]
+    pub created_by: Option<String>,
+}
+
+pub fn parse_bencode(buffer: Vec<u8>) -> Result<Torrent, serde_bencode::Error> {
+    let t = de::from_bytes::<Torrent>(&buffer)?;
+    Ok(t)
 }
